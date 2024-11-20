ac7d6878876fa82e86fe61180291a828b466b0b7
Miguel Angel Rodriguez Feregrino <miguelrdzfere@gmail.com>
2024-11-19 18:41:36 -0600

    Cambios en script de funciones

--- 02_Scripts/Script_prueba_calculos_3_puntos_invertida.R
+++ 02_Scripts/Script_prueba_calculos_3_puntos_invertida.R
@@ -1,10 +1,13 @@
 prueba3puntos.inv <- function(Coeficiente_consistencia, Distancia_zona1, Distancia_zona2, 
-                              gen1_parental = "A", gen2_parental = "B", gen3_parental = "C", 
+                              gen_parental = c("A","B","C"), 
                               total = FALSE, inf.observados = FALSE){
   frec_dobles_recombinantes <- ((Distancia_zona1/100)*(Distancia_zona2/100))*Coeficiente_consistencia
   frec_simples_zona1 <- ((Distancia_zona1/100)-frec_dobles_recombinantes)
   frec_simples_zona2 <- ((Distancia_zona2/100)-frec_dobles_recombinantes)
   frec_parentales <- 1 - (frec_simples_zona2+frec_simples_zona1)
+  gen1_parental <- gen_parental[1]
+  gen2_parental <- gen_parental[2]
+  gen3_parental <- gen_parental[3]
   if (total != FALSE){ # Aqui vamos a poner lo que pasa si total = a un numero
     num_dobles_recombinantes <- frec_dobles_recombinantes*total
     num_simples_zona1 <- frec_simples_zona1*total
@@ -23,7 +26,7 @@ prueba3puntos.inv <- function(Coeficiente_consistencia, Distancia_zona1, Distanc
                      paste(gen1_parental,"-",gen2_parental,"-",gen3_parental),
                      paste(gen1_parental,gen2_parental,"-",gen3_parental),
                      paste(gen1_parental,"-",gen2_parental,gen3_parental,"-")),
-        Observados_esperados = c(num_parentales/2,num_parentales/2,num_simples_zona1/2,num_simples_zona1/2,
+        Esperados = c(num_parentales/2,num_parentales/2,num_simples_zona1/2,num_simples_zona1/2,
                                   num_simples_zona2/2,num_simples_zona2/2,num_dobles_recombinantes/2,num_dobles_recombinantes/2),
         Frecuencia_esperada = c(frec_parentales/2, frec_parentales/2,frec_simples_zona1/2,frec_simples_zona1/2,
                                 frec_simples_zona2/2,frec_simples_zona2/2,frec_dobles_recombinantes/2,frec_dobles_recombinantes/2)
@@ -44,7 +47,7 @@ prueba3puntos.inv <- function(Coeficiente_consistencia, Distancia_zona1, Distanc
                      c(paste(gen1_parental,gen2_parental,"-",gen3_parental,"-","and",gen1_parental,"-",gen2_parental,gen3_parental)),
                      c(paste(gen1_parental,gen2_parental,gen3_parental,"-","and",gen1_parental,"-",gen2_parental,"-",gen3_parental)),
                      c(paste(gen1_parental,gen2_parental,"-",gen3_parental,"and",gen1_parental,"-",gen2_parental,gen3_parental,"-"))),
-        Observados_esperados = c(num_parentales,num_simples_zona1,num_simples_zona2,num_dobles_recombinantes),
+        Esperados = c(num_parentales,num_simples_zona1,num_simples_zona2,num_dobles_recombinantes),
         Frecuencia = c(frec_parentales,frec_simples_zona1,frec_simples_zona2,frec_dobles_recombinantes)
       )
       resultados <- list(
@@ -85,9 +88,7 @@ eje2
 eje3 <- prueba3puntos.inv(0.994035,7,5.03, total = 4000)
 eje3
 
-eje <- prueba3puntos.inv(0.994035,7,5.03,gen1_parental = "K",
-                  gen2_parental = "e",
-                  gen3_parental = "CD",
+eje <- prueba3puntos.inv(0.994035,7,5.03,gen_parental = c("ct","v","cv"),
                   total = 4000, inf.observados = TRUE)
 eje
 
